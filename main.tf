57608c031fbdea5d4faa075cb4d732f9ffe3d736
Chloe Fitzgerald <chloefitzgerald271@gmail.com>
2026-04-08 16:19:54 +0200

    turns out 'skip_requesting_account_id' is necessary maybe

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ terraform {
       skip_credentials_validation = true
       skip_metadata_api_check     = true
       skip_region_validation      = true
+      skip_requesting_account_id  = true
       region                      = "eu-central-003"
       bucket                      = "bb-eu-s3-private-terraform"
       key                         = "actions-test/terraform.tfstate"
