5b313b82588f0d33826b11f2e7d6dadc86e308da
Chloe Fitzgerald <chloefitzgerald271@gmail.com>
2026-04-08 17:18:29 +0200

    keep trying to get backend to work

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
-backend "s3" {
-  endpoints {
-    s3 = "https://s3.eu-central-003.backblazeb2.com"
+  backend "s3" {
+    endpoints {
+      s3 = "https://s3.eu-central-003.backblazeb2.com"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
   }
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_requesting_account_id  = true
-}
   required_providers {
     telmate = {
       source = "telmate/proxmox"
