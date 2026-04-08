ead974fbf9e882158ce63c141c2555a6051d00b9
Chloe Fitzgerald <chloefitzgerald271@gmail.com>
2026-04-08 16:14:24 +0200

    jfoaijwg

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
     backend "s3" {
-    endpoint = "https://s3.eu-central-003.backblazeb2.com"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    region                      = "eu-central-003"
-    bucket                      = "bb-eu-s3-private-terraform"
-    key                         = "actions-test/terraform.tfstate"
+      endpoint = "https://s3.eu-central-003.backblazeb2.com"
+      skip_credentials_validation = true
+      skip_metadata_api_check     = true
+      skip_region_validation      = true
+      region                      = "eu-central-003"
+      bucket                      = "bb-eu-s3-private-terraform"
+      key                         = "actions-test/terraform.tfstate"
   }
   required_providers {
     telmate = {
