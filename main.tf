d1cb19daea73c3589fa5e590109a13475584a9d1
Chloe Fitzgerald <chloefitzgerald271@gmail.com>
2026-04-08 16:04:26 +0200

    cleanup main.tf- remove deprecated features

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    #skip_requesting_account_id  = true
-    #skip_s3_checksum            = true
     region                      = "eu-central-003"
     bucket                      = "bb-eu-s3-private-terraform"
     key                         = "actions-test/terraform.tfstate"
